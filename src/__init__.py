b1288df213a55fd9838cebb5235499af9e6573bd
Grushvv <160276404+Grushvv@users.noreply.github.com>
2025-10-02 23:58:42 +0300

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,1 @@
 """Top-level package for source code (project logic)."""
-from .base import IEmbedder
-from .bow import BOWEmbedder
-from .tfidf import TFIDFEmbedder
-from .word2vec import Word2VecEmbedder
-from .sbert import SentenceBERTEmbedder
-from .openai_embedder import OpenAIEmbedder
-from .factory import EmbedderFactory
